69f1a530d1342ecd2e443600798213466d9e0e43
barrientosjacob704-afk <barrientosjacob704@gmail.com>
2025-09-29 19:22:17 +0100

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,10 @@
+print("hello world")
+#today i am writing my first python program
+print("my name is noah barrientos")
+print("i study in grade 9")
+print("i am 13 years old")
+print("im turning 14 this october")
+num1=int(input("enter your number1:"))
+num2=int(input("enter your number2:"))
+sum=num1+num2
+print(sum)
